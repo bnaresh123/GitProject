62ffdc7ddcbf7d63617a9b17570b18423c46c943
Govardhan <Govardhan>
2016-09-04 19:14:14 +0530

    first commit

---
+++ src/Pack1/TestClass1.java
@@ -0,0 +1,17 @@
+package Pack1;
+
+import org.testng.annotations.Test;
+
+public class TestClass1 {
+	@Test
+	public void TestClass1_Method1(){
+		System.out.println("Executing TestClass1_Method1");
+	}
+	
+	@Test
+	public void TestClass1_Method2(){
+		System.out.println("Executing TestClass1_Method2");
+	}
+	
+	
+}
---
+++ src/Pack1/TestClass2.java
@@ -0,0 +1,16 @@
+package Pack1;
+
+import org.testng.annotations.Test;
+
+public class TestClass2 {
+	@Test
+	public void TestClass2_Method1(){
+		System.out.println("Executing TestClass2_Method1");
+	}
+	
+	@Test
+	public void TestClass2_Method2(){
+		System.out.println("Executing TestClass2_Method2");
+	}
+
+}
