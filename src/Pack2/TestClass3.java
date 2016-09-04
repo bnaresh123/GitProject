5f0907424bcae3ef6cad251cc17595e9251ce029
Govardhan <Govardhan>
2016-09-04 19:23:02 +0530

    I have added test class3

---
+++ src/Pack2/TestClass3.java
@@ -0,0 +1,15 @@
+package Pack2;
+
+import org.testng.annotations.Test;
+
+public class TestClass3 {
+	@Test
+	public void TestClass3_Method1(){
+		System.out.println("Executing TestClass3_Method1");
+	}
+	
+	@Test
+	public void TestClass3_Method2(){
+		System.out.println("Executing TestClass3_Method2");
+	}
+}
